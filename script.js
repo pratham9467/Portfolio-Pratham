17b365f9f145dcc6dd54dfee8aae37df3a4d7853
pratham9467 <76515135+pratham9467@users.noreply.github.com>
2023-10-07 19:49:25 +0530

    personal portfolio

--- script.js
+++ script.js
@@ -7,14 +7,31 @@ function PageTransitions() {
   //Button click active class
   for (let i = 0; i < sectBtn.length; i++) {
     sectBtn[i].addEventListener("click", function () {
-      let currentBtn = document.querySelectorAll(".active-btn");
-      currentBtn[0].className = currentBtn[0].className.replace(
-        "active-btn",
-        ""
-      );
-      this.className += " active-btn";
+      let currentBtn = document.querySelector(".active-btn");
+      currentBtn.classList.remove("active-btn");
+      this.classList.add("active-btn");
     });
   }
+
+  //section active
+  allSections.addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      //remove selected from other btns
+      sectBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      e.target.classList.add("active");
+
+      //remove section active classes and add to clicked one
+
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
+  });
 }
 
 PageTransitions();
